0e5b33626f2d4f008bd0a908c75f5cc0bff61cdf
Michal Miklas <michal.miklas@ie.ibm.com>
2018-12-30 21:53:55 +0000

    feat(ui-shell): make side-nav extend ContentSwitcher

--- src/components/ui-shell/side-nav.js
+++ src/components/ui-shell/side-nav.js
@@ -1,8 +1,12 @@
-import mixin from '../../globals/js/misc/mixin';
-import createComponent from '../../globals/js/mixins/create-component';
-import initComponentBySearch from '../../globals/js/mixins/init-component-by-search';
+import on from '../../globals/js/misc/on';
+import settings from '../../globals/js/settings';
+import ContentSwitcher from '../tabs/tabs';
 
-class SideNav extends mixin(createComponent, initComponentBySearch) {
+class SideNav extends ContentSwitcher {
+  constructor(element, options) {
+    super(element, options);
+    this.manage(on(this.element, 'keydown', this._handleKeyDown));
+  }
   /**
    * The map associating DOM element and copy button UI instance.
    * @member SideNav.components
@@ -12,15 +16,23 @@ class SideNav extends mixin(createComponent, initComponentBySearch) {
 
   /**
    * The component options.
-   * If `options` is specified in the constructor, {@linkcode SideNav.create .create()}, or {@linkcode SideNav.init .init()},
-   * properties in this object are overriden for the instance being create and how {@linkcode SideNav.init .init()} works.
+   * If `options` is specified in the constructor,
+   * {@linkcode SideNav.create .create()}, or
+   * {@linkcode SideNav.init .init()},
+   * properties in this object are overriden for the instance being create and
+   * how {@linkcode SideNav.init .init()} works.
    * @member SideNav.options
    * @type {Object}
    * @property {string} selectorInit The data attribute to find side navs.
    */
-  static options /* #__PURE_CLASS_PROPERTY__ */ = {
-    selectorInit: '[data-side-nav]',
-  };
+  static get options() {
+    const { prefix } = settings;
+    return Object.assign(Object.create(ContentSwitcher.options), {
+      selectorInit: '[data-side-nav]',
+      selectorButton: `.${prefix}--side-nav__link`,
+      classActive: `${prefix}--side-nav__link--current`,
+    });
+  }
 }
 
 export default SideNav;
